8ac222c292f2e0e2f5af25cfde77da6c28960425
Taylor Masiello <masiellotay@gmail.com>
2021-06-13 20:43:24 -0700

    Global State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Dr Appt",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Mtg at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shop",
+      day: "Feb 5th at 2:30p",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
