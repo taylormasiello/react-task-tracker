c4f2593e6ae59157bd7b7bd3a600ed8d546dc1a9
Taylor Masiello <masiellotay@gmail.com>
2021-06-13 21:51:39 -0700

    Delete Task

--- src/App.js
+++ src/App.js
@@ -24,10 +24,19 @@ function App() {
     },
   ]);
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
